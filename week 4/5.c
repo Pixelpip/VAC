879a3eae165a75b909af163ad2561f76b00ed091
Pixelpip <athulkamathg5@gmail.com>
2023-12-07 21:46:29 +0530

    1

--- week 4/5.c
+++ week 4/5.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-void main(){
+
+void 
+
+
+void main(){    
     int r;
     printf("enter the number of rows you would like to print:");
     scanf("%d",&r);
-    int space=1;
     int ASCII=65;
     for(int i=0;i<r;i++){
-        if(r>=1){
-            if(r)
-          printf("%c ",ASCII);
-          ASCII++;
-        }
+        
     }
-}
+    
+        
+    }
